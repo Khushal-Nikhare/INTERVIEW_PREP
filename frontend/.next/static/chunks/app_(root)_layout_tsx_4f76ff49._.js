38aad473c539a9f63e0f4af328887cde046c663d
Khushal-Nikhare <kushalnikhare@gmail.com>
2026-02-23 22:41:30 +0530

    Removed .next build files and updated gitignore

--- frontend/.next/static/chunks/app_(root)_layout_tsx_4f76ff49._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_02133117._.js",
-  "static/chunks/_f245a6cc._.js"
-],
-    source: "dynamic"
-});
